de01d86a1de132d19bb5ac6fc37d1f30f1536e08
Brian Duong <brianminhduong@gmail.com>
2021-10-22 00:17:43 +0000

    Added Template Literals version of return result

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,15 @@ const assertEqual = function(actual, expected) { //Nameless function assigned to
 
   if (actual === expected) { //Compares variables actual and expected to see if they are equal
 
-    return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message
+    // return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message without Template Literals
+
+    return console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`); //Prints out pass message with Template Literals
 
   }
 
-  return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message
+  // return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message without Template Literals
+
+  return console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`); //Prints out fail message with Template Literals
 
 };
 
