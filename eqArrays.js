904625970633535f1167191ba3500264a76a3d6c
Brian Duong <brianminhduong@gmail.com>
2021-10-23 07:49:51 +0000

    Function that returns true or false if all elements are equal

---
+++ eqArrays.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) { //Nameless function assigned to the variable assertEqual
+
+  if (actual === expected) { //Compares variables actual and expected to see if they are equal
+
+    // return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message without Template Literals
+
+    return console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`); //Prints out pass message with Template Literals
+
+  }
+
+  // return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message without Template Literals
+
+  return console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`); //Prints out fail message with Template Literals
+
+};
+
+const eqArrays = function(arr1, arr2) { //Nameless function assigned to the variable eqArrays
+
+  /*
+  Parameters
+  arr1 - array of elements
+  arr2 - array of elements
+
+  Returns
+  True if all elements are equal
+  False if any of the elements are not equal
+  */
+
+  if (arr1.length !== arr2.length) { //Check to see if both arrays are equal length
+
+    return false;
+
+  }
+
+  for(let i = 0; i < arr1.length; i++) { //Iterates over the length of arr1
+
+    if (arr1[i] !== arr2[i]) { //Checks at the same index for both arrays if the elements are equal
+
+      return false;
+
+    }
+  }
+
+  return true; //Passes all tests, elements are equal
+};
+
+//Test Case
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //All elements are equal 
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true); //One element is different
+assertEqual(eqArrays([1, 2, '3'], [1, 2, '3']), true); //Checks string and integers
+assertEqual(eqArrays([1, 2, '3'], [1, 2]), true); //Checks different lengths
