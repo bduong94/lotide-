f7c59f6e618b98b2637564746847d52365e99e4c
Brian Duong <brianminhduong@gmail.com>
2021-11-09 01:01:29 +0000

    Refactored tail.js

--- tail.js
+++ tail.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) { //Nameless function assigned to the variable assertEqual
-
-  if (actual === expected) { //Compares variables actual and expected to see if they are equal
-
-    // return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message without Template Literals
-
-    return console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`); //Prints out pass message with Template Literals
-
-  }
-
-  // return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message without Template Literals
-
-  return console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`); //Prints out fail message with Template Literals
-
-};
-
 const tail = function(words) { //Nameless function assigned to the variable tail
 
   if (words === [] || words.length === 1) { //Checks to see if words is an empty string or has only one element in the array
@@ -26,10 +10,4 @@ const tail = function(words) { //Nameless function assigned to the variable tail
 
 };
 
-//Test Case
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-console.log(tail([])); //Tests empty array
-console.log(tail(["one"])); //Tests one element array
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+//Test Case
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+console.log(tail([])); //Tests empty array
+console.log(tail(["one"])); //Tests one element array
