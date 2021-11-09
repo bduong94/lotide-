5a866cef697270e4ac98adb18d4e5d30088d5d4a
Brian Duong <brianminhduong@gmail.com>
2021-11-09 02:03:15 +0000

    Added Mocha and Chai to Lotide directory, refactored headTest.js to use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,22 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-//Test Cases
-assertEqual(head([5,6,7]), 5); //Test Case with integers
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //Test Case with strings
-console.log(head([])); //Test case with empty string
-console.log(head([1])); //Test case with 1 element only
+describe('head', () => {
+
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
