b900bf73b3e62205434bf4cba25a9711410344c5
Brian Duong <brianminhduong@gmail.com>
2021-11-09 01:29:46 +0000

    Refactored middle and eslint

--- middle.js
+++ middle.js
@@ -1,51 +1,3 @@
-const assertArraysEqual = function(arr1, arr2) { //Nameless function assigned to the variable assertArraysEqual
-
-  let result = eqArrays(arr1, arr2);
-
-  if (result) { //Compares variables actual and expected to see if they are equal
-
-    // return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message without Template Literals
-
-    return console.log(`✅✅✅ Assertion Passed: "${arr1}" === "${arr2}"`); //Prints out pass message with Template Literals
-
-  }
-
-  // return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message without Template Literals
-
-  return console.log(`🛑🛑🛑 Assertion Failed: "${arr1}" !== "${arr2}"`); //Prints out fail message with Template Literals
-
-};
-
-const eqArrays = function(arr1, arr2) { //Nameless function assigned to the variable eqArrays
-
-  /*
-  Parameters
-  arr1 - array of elements
-  arr2 - array of elements
-
-  Returns
-  True if all elements are equal
-  False if any of the elements are not equal
-  */
-
-  if (arr1.length !== arr2.length) { //Check to see if both arrays are equal length
-
-    return false;
-
-  }
-
-  for (let i = 0; i < arr1.length; i++) { //Iterates over the length of arr1
-
-    if (arr1[i] !== arr2[i]) { //Checks at the same index for both arrays if the elements are equal
-
-      return false;
-
-    }
-  }
-
-  return true; //Passes all tests, elements are equal
-};
-
 const middle = function(arr) { //Nameless function assigned to the variable middle
   
   /*
@@ -81,10 +33,4 @@ const middle = function(arr) { //Nameless function assigned to the variable midd
 
 };
 
-//Test Cases
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2,3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+module.exports = middle;
