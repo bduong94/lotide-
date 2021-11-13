4154f99a699e8e806416cb9bcbd62934fbedcffb
Brian Duong <brianminhduong@gmail.com>
2021-11-13 20:57:32 +0000

    Add test cases for assertEqualTest.js

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,20 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-//Test Code
-assertEqual("Coders", "Coders"); //Identical Strings
-assertEqual("Lighthouse Labs", "Bootcamp"); //Non-identical strings
-assertEqual(1, 1); //Identical numbers
-assertEqual(5, 4); //Non-identical numbers
+describe('Test cases for assertEqual.js', () => {
+  it('"Coders" and "Coders" returns ✅✅✅ Assertion Passed', () => {
+    assert.strictEqual(assertEqual("Coders", "Coders"),console.log(`✅✅✅ Assertion Passed: "Coders" === "Coders"`))
+  })
+
+  it('"Lighthouse Labs" and "Bootcamp" returns 🛑🛑🛑 Assertion Failed: ', () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"),console.log(`🛑🛑🛑 Assertion Failed: "Lighthouse Labs" !== "Bootcamp"`))
+  })
+
+  it('1 and 1 returns ✅✅✅ Assertion Passed', () => {
+    assert.strictEqual(assertEqual(1, 1),console.log(`✅✅✅ Assertion Passed: "${1}"" === "${1}""`))
+  })
+
+  it('5 and 4 returns 🛑🛑🛑 Assertion Failed: ', () => {
+    assert.strictEqual(assertEqual(5, 4),console.log(`🛑🛑🛑 Assertion Failed: "${5}" !== "${4}"`))
+  })
+})
