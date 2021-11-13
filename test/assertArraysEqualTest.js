2a484359a9669566a78268274deb95aad1c86f56
Brian Duong <brianminhduong@gmail.com>
2021-11-13 21:21:44 +0000

    Add test cases for assertEquals

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,14 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
-//Test Case
-assertArraysEqual([1, 2, 3], [1, 2, 3]); //All elements are equal
-assertArraysEqual([1, 2, 3], [1, 2, '3']); //One element is differArraysent
-assertArraysEqual([1, 2, '3'], [1, 2, '3']); //Checks string and integers
-assertArraysEqual([1, 2, '3'], [1, 2]); //Checks different lengths
+describe('Test cases for assertArraysEqual', () => {
+
+  it('[1, 2, 3] and [1, 2, 3] returns ✅✅✅ Assertion Passed', () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), console.log(`✅✅✅ Assertion Passed: "${[1, 2, 3]}" === "${[1, 2, 3]}"`));
+  });
+  
+  it('[1, 2, 3] and [1, 2, "3"] returns ✅✅✅ Assertion Passed', () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, "3"]), console.log(`🛑🛑🛑 Assertion Failed: "${[1, 2, 3]}" !== "${[1, 2, '3']}"`));
+  });
+
+});
