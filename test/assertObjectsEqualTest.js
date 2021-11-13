2c798a29c6969bbe5d126f40c9d065b8b0f53f10
Brian Duong <brianminhduong@gmail.com>
2021-11-13 20:46:39 +0000

    Add test case for failing

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -4,6 +4,10 @@ const inspect = require('util').inspect;
 
 describe('Tests for assertObjectsEqual', () => {
   it('{a: "1", b: "2"} and {b: "2", a: "1"} should return ✅✅✅ Assertion Passed:', () => {
-    assert.strictEqual(assertObjectEqual({a: "1", b: "2"}, {a: "1", b: "2"}, `✅✅✅ Assertion Passed: "${inspect({a: "1", b: "2"})}" === "${inspect({a: "1", b: "2"})}"`));
+    assert.strictEqual(assertObjectEqual({a: "1", b: "2"}, {b: "2", a: "1"}, `✅✅✅ Assertion Passed: "${inspect({a: "1", b: "2"})}" === "${inspect({b: "2", a: "1"})}"`));
+  });
+  
+  it('{a: "1", b: "2"} and {b: "2", a: "3"} should return 🛑🛑🛑 Assertion Failed:', () => {
+    assert.strictEqual(assertObjectEqual({a: "1", b: "2"}, {b: "2", a: "3"}, `🛑🛑🛑 Assertion Failed: "${inspect({a: "1", b: "2"})}" === "${inspect({a: "1", b: "3"})}"`));
   });
 });
