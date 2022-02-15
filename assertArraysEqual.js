53fe9bc2352364516f989747c9d71c9b106c619b
Brian Duong <brianminhduong@gmail.com>
2022-02-14 20:28:17 -0500

    Testing push

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,19 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(arr1, arr2) { //Nameless function assigned to the variable assertArraysEqual
+const assertArraysEqual = function (arr1, arr2) {
+  //Nameless function assigned to the variable assertArraysEqual
 
   let result = eqArrays(arr1, arr2);
 
-  if (result) { //Compares variables actual and expected to see if they are equal
-
-    // return console.log('✅✅✅ Assertion Passed: "' + actual + '" === "' + expected + '"'); //Prints out pass message without Template Literals
+  if (result) {
+    //Compares variables actual and expected to see if they are equal
 
     return console.log(`✅✅✅ Assertion Passed: "${arr1}" === "${arr2}"`); //Prints out pass message with Template Literals
-
   }
 
   // return console.log('🛑🛑🛑 Assertion Failed: "' + actual + '" !== "' + expected + '"'); //Prints out fail message without Template Literals
 
   return console.log(`🛑🛑🛑 Assertion Failed: "${arr1}" !== "${arr2}"`); //Prints out fail message with Template Literals
-
 };
 
 module.exports = assertArraysEqual;
